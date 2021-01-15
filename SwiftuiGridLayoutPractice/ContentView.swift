1699c6dcfebe1ab2da0569842de7394f9c41fac7
PageKallop <PageMarie78@gmail.com>
2021-01-15 12:14:12 -0600

    Created array layout Added slider

--- SwiftuiGridLayoutPractice/ContentView.swift
+++ SwiftuiGridLayoutPractice/ContentView.swift
@@ -8,10 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let animals = [["🐶🐼"],["🐭🐹"],["🐰🦊"],["🐻🐻‍❄️"]]
+    @State private var sliderValue: CGFloat = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            
+            
+        VStack{
+            Slider(value: $sliderValue, in: 1...CGFloat(animals.count), step: 1)
+            Text(String(format: "%.0f", self.sliderValue))
+                .font(.system(size: 20))
+                .fontWeight(.bold)
+                .padding()
+                .background(Color.purple)
+                .foregroundColor(Color.white)
+                .clipShape(Circle())
+            
+            
+            List(self.animals, id: \.self) { animalPair in
+                ForEach(animalPair, id: \.self) { animal in
+                    Text(animal)
+                        .font(.system(size: 100))
+                }
+            }
+            
+        }
+        
+        .navigationBarTitle("Animal Looks")
     }
+   }
 }
 
 struct ContentView_Previews: PreviewProvider {
